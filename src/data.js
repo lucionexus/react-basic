ab5d14e923d13c9347f7f235fca5df8b56bae97a
lucionexus <bossreyshan@gmail.com>
2022-04-21 14:56:31 +0700

    masukan file

---
+++ src/data.js
@@ -0,0 +1,44 @@
+const data = [
+  {
+    mal_id: 5114,
+    rank: 1,
+    title: "Fullmetal Alchemist: Brotherhood",
+    url: "https://myanimelist.net/anime/5114/Fullmetal_Alchemist__Brotherhood",
+    image_url:
+      "https://cdn.myanimelist.net/images/anime/1223/96541.jpg?s=faffcb677a5eacd17bf761edd78bfb3f",
+    type: "TV",
+    episodes: 64,
+    start_date: "Apr 2009",
+    end_date: "Jul 2010",
+    members: 2649743,
+    score: 9.16,
+  },
+  {
+    mal_id: 28977,
+    rank: 2,
+    title: "Gintama\u00b0",
+    url: "https://myanimelist.net/anime/28977/Gintama\u00b0",
+    image_url:
+      "https://cdn.myanimelist.net/images/anime/3/72078.jpg?s=e9537ac90c08758594c787ede117f209",
+    type: "TV",
+    episodes: 51,
+    start_date: "Apr 2015",
+    end_date: "Mar 2016",
+    members: 475052,
+    score: 9.1,
+  },
+  {
+    mal_id: 38524,
+    rank: 3,
+    title: "Shingeki no Kyojin Season 3 Part 2",
+    url: "https://myanimelist.net/anime/38524/Shingeki_no_Kyojin_Season_3_Part_2",
+    image_url:
+      "https://cdn.myanimelist.net/images/anime/1517/100633.jpg?s=4540a01b5883647ade494cd28392f100",
+    type: "TV",
+    episodes: 10,
+    start_date: "Apr 2019",
+    end_date: "Jul 2019",
+    members: 1560848,
+    score: 9.09,
+  },
+];
